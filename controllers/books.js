7ee0793724f27406ae3f90086227def419235155
Evy <evytchr@gmail.com>
2024-05-08 16:43:39 +0200

    feat(route) : POST pour les livres

--- controllers/books.js
+++ controllers/books.js
@@ -1,17 +1,22 @@
 const Book = require('../models/Book')
 
-
-
 exports.createBook = (req, res, next) => {
     const bookObject = JSON.parse(req.body.book)
     delete bookObject._id
     delete bookObject._userId
-    const book = new Book({
-        ...bookObject,
-        userId: req.auth.userId,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    Book.findOne({title: bookObject.title, author: bookObject.author, year: bookObject.year})
+    .then (book => {
+        if (book) {
+            return res.status(409).json({message: 'Livre déjà enregistré'})
+        }
+        const newbook = new Book({
+            ...bookObject,
+            userId: req.auth.userId,
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        })
+        newbook.save()
+        .then(() => res.status(201).json({message: 'Livre enregistré'}))
+        .catch(error => {res.status(400).json({error})})
     })
-    book.save()
-    .then(() => res.status(201).json({message: 'Livre enregistré'}))
-    .catch(error => {res.status(400).json({error})})
+    .catch(error => res.status(500).json({error}))
 }
